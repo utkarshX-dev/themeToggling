f80bc7374558edac0b3ebfd6ce3767fadfb3fbf7
utkarshBhandari <utkarshX-dev@users.noreply.github.com>
2025-08-18 00:01:07 +0530

    theme toggle simple

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,32 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [dark, setDark] = useState<boolean>(localStorage.getItem("theme") == "dark");
+
+  useEffect(() => {
+    const html = document.querySelector("html") as HTMLElement;
+    if (dark) {
+      localStorage.setItem("theme", "dark");
+      html.classList.add("dark");
+    } else {
+      localStorage.removeItem("theme");
+      html.classList.remove("dark");
+    }
+    const text: string = dark ? "dark theme" : "light theme";
+    console.log("current theme : ", text);
+  }, [dark]);
+  const handleClick = (): void => {
+    setDark(!dark);
+    console.log("theme change");
+  };
   return (
     <>
-       <div className="max-w-sm rounded-2xl shadow-lg bg-white dark:bg-gray-800 p-6 m-auto mt-10">
-      <div className="text-6xl mb-4">🌸</div>
-      <h2 className="text-xl font-semibold text-gray-800 dark:text-gray-100">
-        Card Title
-      </h2>
-      <p className="mt-2 text-gray-600 dark:text-gray-300">
-        This is a simple card with an emoji instead of an image. You can style it however you like.
-      </p>
-      <div className="text-center">
-      <button className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors">
-        Action
+      <button
+        className="border-2 border-amber-600 m-4 p-3 rounded-2xl bg-gray-500"
+        onClick={handleClick}
+      >
+        Change Theme
       </button>
-      </div>
-    </div>
     </>
   );
 }
